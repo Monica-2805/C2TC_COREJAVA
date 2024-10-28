b6f7614d1f916b89b59b12a2c6213d3826776e37
Vishnu R <Vishnu R@Monicaa>
2024-10-28 19:52:49 +0530

    I Need to Commit

--- C2TC_MONICA/src/com/tns/dayone/FirstProgram.java
+++ C2TC_MONICA/src/com/tns/dayone/FirstProgram.java
@@ -3,8 +3,7 @@ package com.tns.dayone;
 public class FirstProgram {
 	public static void main(String[]  args) {
 		int $number = 20; //starts with $
-		System.out.println("value of the number
-		variable is : "+ $number);
+		System.out.println("value of the numbervariable is : "+ $number);
 		String studentName="Aniket"; //use camelcase
 		System.out.println("value is : "+
 		studentName);

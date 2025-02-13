7488c14314fbd174d509c5ec7741c72287a053c7
Monica-2805 <monicarajaram2@gmail.com>
2025-02-13 11:40:59 +0530

    Add files via upload
    recent

---
+++ shoppingmall/OrderItem.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.monica.project.springboot;
+
+	package com.tns.ifet.monica.project.shoppingmall;
+
+	import jakarta.persistence.Entity;
+	import jakarta.persistence.GeneratedValue;
+	import jakarta.persistence.GenerationType;
+	import jakarta.persistence.Id;
+	import jakarta.persistence.ManyToOne;
+	import jak
